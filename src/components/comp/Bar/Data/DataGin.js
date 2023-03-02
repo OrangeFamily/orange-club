33846c04726e0373c30bcb91c2d55adb878d4e95
Олексій Демидов <orange.family.cn@gmail.com>
2023-03-02 09:18:01 +0200

    fix

--- src/components/comp/Bar/Data/DataGin.js
+++ src/components/comp/Bar/Data/DataGin.js
@@ -6,28 +6,13 @@ const GinMass=[
         src: require('../img/gordons.webp'),
         text:"50мл - 91 грн",
     },
-    {
+        {
         id:2,
-        title:"Харана Б'янко (Великобританія) 1л",
-        price:"1780 грн",
-        src: require('../img/JaranaB.png'),
-        text:"50мл - 89 грн",
-    },
-    {
-        id:3,
-        title:"Харана Репосадо (Великобританія) 1л",
-        price:"1780 грн",
-        src: require('../img/JaranaR.png'),
-        text:"50мл - 89 грн",
-    },
-    {
-        id:4,
         title:"Тенкері №ТЕН (Великобританія) 1л",
         price:"3200 грн",
         src: require('../img/Tanqueray.png'),
         text:"50мл - 160 грн",
     },
-    
 ]
 
 export default GinMass;
--- src/components/comp/Bar/Data/DataTekila.js
+++ src/components/comp/Bar/Data/DataTekila.js
@@ -13,9 +13,20 @@ const TekilaMass=[
         src: require('../img/jkg.webp'),
         text:"50мл - 86грн",
     },
-    
-    
-    
+    {
+        id:3,
+        title:"Харана Б'янко (Великобританія) 1л",
+        price:"1780 грн",
+        src: require('../img/JaranaB.png'),
+        text:"50мл - 89 грн",
+    },
+    {
+        id:4,
+        title:"Харана Репосадо (Великобританія) 1л",
+        price:"1780 грн",
+        src: require('../img/JaranaR.png'),
+        text:"50мл - 89 грн",
+    },
 ]
 
 export default TekilaMass;
