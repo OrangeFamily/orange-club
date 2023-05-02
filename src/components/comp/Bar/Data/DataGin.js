cede79ece6fe43fbdb92f8c9a53262f50f71f452
Олексій Демидов <orange.family.cn@gmail.com>
2023-05-02 15:33:09 +0300

    fix

--- src/components/comp/Bar/Data/DataGin.js
+++ src/components/comp/Bar/Data/DataGin.js
@@ -1,18 +1,25 @@
 const GinMass=[
     {
-        id:1,
-        title:"Гордон'с Лондон Драй (Великобританія) 1л",
-        price:"1820 грн",
+        id: 1,
+        title: "Гордон'с Лондон Драй (Великобританія) 1л",
+        price: '1820 грн',
         src: require('../img/gordons.webp'),
-        text:"50мл - 91 грн",
-    },
-        {
-        id:2,
-        title:"Тенкері №ТЕН (Великобританія) 1л",
-        price:"3200 грн",
+        text: '50мл - 91 грн',
+      },
+      {
+        id: 2,
+        title: "Гордон'с оранж (Великобританія) 1л",
+        price: '1820 грн',
+        src: require('../img/ginorange.png'),
+        text: '50мл - 91 грн',
+      },
+      {
+        id: 3,
+        title: 'Тенкері №ТЕН (Великобританія) 1л',
+        price: '3200 грн',
         src: require('../img/Tanqueray.png'),
-        text:"50мл - 160 грн",
-    },
+        text: '50мл - 160 грн',
+      },
 ]
 
 export default GinMass;
