89b729cbf8cea44961f338988bdd18a6122c6789
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2024-07-29 18:29:30 +0300

    fix

--- src/components/comp/Cocotail/Data/DataPartyMix.js
+++ src/components/comp/Cocotail/Data/DataPartyMix.js
@@ -3,22 +3,22 @@ const PartyMixMass=[
         id:1,
         title:"Party Mix Whiskey",
         price:"2000 грн",
-        src: require('../img/1.webp'),
+        src: require('../img/partybells.png'),
         text:"Red Bull - 6шт., віскі Bell`s - 1л",
     },
     {
         id:2,
         title:"Party Mix Jagermeister",
         price:"2000 грн",
-        src: require('../img/1.webp'),
+        src: require('../img/partyjager.png'),
         text:"Red Bull - 6шт., Jägermeister - 1л",
     },
     {
         id:3,
         title:"Party Mix vodka",
         price:"830 грн",
-        src: require('../img/1.webp'),
-        text:"Red Bull - 6шт., Nemiroff - 0.5л",
+        src: require('../img/partyvodka.png'),
+        text:"Red Bull - 6шт., Перша Гільдія Поважна - 0.5л",
     },
 ]
 
