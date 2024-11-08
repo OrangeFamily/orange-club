dfd746e68b4fc371c9b104f4f0ca0d80c769d275
Олексій Демидов <agdemidof@gmail.com>
2024-11-08 16:10:38 +0200

    Update DataShot.js

--- src/components/comp/Bar/Data/DataShot.js
+++ src/components/comp/Bar/Data/DataShot.js
@@ -2,14 +2,14 @@ const ShotMass=[
     {
         id:1,
         title:"Єгермайстер (Германія) 1л",
-        price:"1800 грн",
+        price:"1900 грн",
         src: require('../img/jager.webp'),
-        text:"50мл - 90грн",
+        text:"50мл - 95грн",
     },
     {
         id:2,
         title:"Єгермайстер (Германія) 0,5л",
-        price:"900 грн",
+        price:"950 грн",
         src: require('../img/jager.webp'),
         text:"",
     },
