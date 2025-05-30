06040eb99c5cc6488a237a108d9018a03262dbf0
Олексій Демидов <agdemidof@gmail.com>
2025-05-30 13:28:24 +0300

    fix

--- src/components/comp/Kitchen/Data/DataAssorti.js
+++ src/components/comp/Kitchen/Data/DataAssorti.js
@@ -1,57 +1,59 @@
-const AssortiMass=[
-    {
-        id:6,
-        title:"оливки 80г",
-        price:"80 грн",
-        src: require('../img/olivki.png'),
-     
-    },
-    {
-        id:7,
-        title:"маслини 80г",
-        price:"80 грн",
-        src: require('../img/maslini.png'),
-        
-    },
-    {
-        id:1,
-        title:"м'ясна тарілка 200г",
-        price:"295 грн",
-        src: require('../img/meattarelka.webp'),
-        text:"Шинка, Ковбаса СК, бастурма, хамон",
-    },
-    {
-        id:2,
-        title:"тарілка під пиво 200г",
-        price:"290 грн",
-        src:require('../img/tarpidbear.png'),
-        text:"курка в'ялена, хамон, бастурма, фісташки",
-    },
-    
-    {
-        id:3,
-        title:"тарілка під горілку 150г/30г",
-        price:"200 грн",
-        src:require('../img/tovodka.webp'),
-        text:"сало, підчеревок, корнішони, гірчиця",
-    },
-    {
-        id:4,
-        title:"сирна тарілка 200г/30г",
-        price:"289 грн",
-        src:require('../img/tarelkachees.webp'),
-        text:"гауда, пармезан, брі, дорблю, мед",
-    },
-    {
-        id:5,
-        title:"фруктова тарілка 480г",
-        price:"250 грн",
-        src:require('../img/fruttar.webp'),
-        text:"Апельсин, грейпфрут, виноград, ківі, груша, мараскінова вишня",
-    },
-   
-    
-    
-]
+const AssortiMass = [
+  {
+    id: 6,
+    title: 'оливки 80г',
+    price: '80 грн',
+    src: require('../img/olivki.png'),
+  },
+  {
+    id: 7,
+    title: 'маслини 80г',
+    price: '80 грн',
+    src: require('../img/maslini.png'),
+  },
+  {
+    id: 1,
+    title: "м'ясна тарілка 200г",
+    price: '330 грн',
+    src: require('../img/meattarelka.png'),
+    text: 'Шинка, Ковбаса СК, бастурма, хамон',
+  },
+  {
+    id: 2,
+    title: 'тарілка під пиво 200г',
+    price: '290 грн',
+    src: require('../img/tarpidbear.png'),
+    text: "курка в'ялена, хамон, бастурма, фісташки",
+  },
+
+  {
+    id: 3,
+    title: 'тарілка під горілку 150/30/30',
+    price: '200 грн',
+    src: require('../img/tovodka.png'),
+    text: 'сало, підчеревок, корнішони, гірчиця, хрін, хліб бородинський, цибуля',
+  },
+  {
+    id: 4,
+    title: 'сирна тарілка 350г/50г',
+    price: '360 грн',
+    src: require('../img/tarelkachees.png'),
+    text: 'гауда, пармезан, брі, дорблю, мед, виноград, груша',
+  },
+  {
+    id: 5,
+    title: 'фруктова тарілка 480г',
+    price: '250 грн',
+    src: require('../img/fruttar.webp'),
+    text: 'Апельсин, грейпфрут, виноград, ківі, груша, мараскінова вишня',
+  },
+  {
+    id: 8,
+    title: 'Оселедець з картоплею та цибулею 80/100/50',
+    price: '150 грн',
+    src: require('../img/oseledecKart.png'),
+    text: 'філе оселедця, картопля, маринована цибуля, кріп',
+  },
+];
 
 export default AssortiMass;
