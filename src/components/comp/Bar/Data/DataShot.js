0ad8f5a6bbdf6843fa9a9f929d90415390f5ee2c
Олексій Демидов <agdemidof@gmail.com>
2025-10-17 13:55:00 +0300

    fix

--- src/components/comp/Bar/Data/DataShot.js
+++ src/components/comp/Bar/Data/DataShot.js
@@ -1,17 +1,24 @@
 const ShotMass=[
     {
         id:1,
-        title:"Єгермайстер (Германія) 1л",
+        title:"Jägermeister (Німеччина) 1л",
         price:"2200 грн",
         src: require('../img/jager.webp'),
         text:"50мл - 110 грн",
     },
     {
         id:2,
-        title:"Єгермайстер (Германія) 0,5л",
+        title:"Jägermeister (Німеччина) 0,5л",
         price:"1100 грн",
         src: require('../img/jager.webp'),
         text:"",
+    },
+    {
+        id:4,
+        title:"Jägermeister orange (Німеччина) 1л",
+        price:"3400 грн",
+        src: require('../img/Jägermeisterorange.png'),
+        text:"50мл - 170 грн",
     },
     // {
     //     id:3,
